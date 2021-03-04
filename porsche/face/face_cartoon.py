de97262cdebd35d4396bf35313017099df7ef386
edvardzeng <edvard_hua@live.com>
2021-03-04 17:27:36 +0800

    update

--- porsche/face/face_cartoon.py
+++ porsche/face/face_cartoon.py
@@ -8,6 +8,7 @@
 
 import os
 import cv2
+import json
 import onnxruntime
 import numpy as np
 
@@ -19,11 +20,13 @@ from porsche.utils.army_knife import get_porsche_base_path
 class FaceCartoon:
     MODEL_PATH = "models/student_gan_0902.onnx"
     BATCH_PATH = "models/student_gan_0902_dynamic_axes.onnx"
+    DEFAULT_LUT_PATH = "models/default_lut.png"
 
     def __init__(self, bg_lut_file=None, custom_bg_func=None, model_path=None):
         self.fd = FaceDetector(need_expand=True)
         self.sehead = Segmentation(type="head")
         self.seskin = Segmentation(type="skin")
+        self.model_path = model_path
         if model_path is not None:
             self.cartoon = onnxruntime.InferenceSession(model_path)
         else:
@@ -35,7 +38,9 @@ class FaceCartoon:
         if bg_lut_file is not None:
             self.lut_img = cv2.imread(bg_lut_file)
         else:
-            self.lut_img = None
+            self.lut_img = cv2.imread(self.DEFAULT_LUT_PATH)
+
+        self.call_counter = 0
 
     def bg_lut(self, frame):
         dest_img = frame.copy()
@@ -59,7 +64,8 @@ class FaceCartoon:
         return dest_img
 
     def batch_infer(self, frames):
-        self.cartoon = onnxruntime.InferenceSession(os.path.join(get_porsche_base_path(), self.BATCH_PATH))
+        if self.model_path is None:
+            self.cartoon = onnxruntime.InferenceSession(os.path.join(get_porsche_base_path(), self.BATCH_PATH))
 
         inp_frames = []
         for f in frames:
@@ -80,7 +86,7 @@ class FaceCartoon:
             cartoon_face = cartoon_face.astype(np.uint8)
             cartoon_face = cartoon_face[:, :, ::-1]
             ori_h, ori_w, _ = frames[ind].shape
-            ret_frame.append(cv2.resize(cartoon_face, (ori_w, ori_h)))
+            ret_frame.append(cv2.resize(cartoon_face, (ori_w, ori_h), interpolation=cv2.INTER_LANCZOS4))
         return ret_frame
 
     def infer(self, frame):
@@ -94,10 +100,10 @@ class FaceCartoon:
 
         cartoon_face = cartoon_face.astype(np.uint8)
         cartoon_face = cartoon_face[:, :, ::-1]
-        return cv2.resize(cartoon_face, (ori_w, ori_h))
+        return cv2.resize(cartoon_face, (ori_w, ori_h), interpolation=cv2.INTER_LANCZOS4)
 
     def input_preprocess(self, img):
-        img = cv2.resize(img, (self.inp_w, self.inp_h))
+        img = cv2.resize(img, (self.inp_w, self.inp_h), interpolation=cv2.INTER_LANCZOS4)
         nor = img / 255.0
         inp = (nor - 0.5) / 0.5
         inp = inp[:, :, ::-1]
@@ -144,27 +150,85 @@ class FaceCartoon:
         mean_img = cv2.pyrMeanShiftFiltering(img, spatial_window_radius, color_window_radius)
         return self.__blend_edges(mean_img, edge)
 
-    def __call__(self, frame, seg=True, blend=True, proc_bg=True, detect_face=True):
+    def set_config(self, seg=True, blend=True, proc_bg=True, detect_face=True, lut_on_cartoon_face=False,
+                   only_seg=False, face_bbox_result=None):
+        """
+        预先设置好运行时设置而不是在 call 的时候传参设置
+        :param seg:
+        :param blend:
+        :param proc_bg:
+        :param detect_face:
+        :param lut_on_cartoon_face:
+        :param only_seg:
+        :param face_bbox_result: [(x, y, w, h), ...]
+        :return:
+        """
+        self.apply_seg = seg
+        self.apply_blend = blend
+        self.apply_proc_bg = proc_bg
+        self.apply_detect_face = detect_face
+        self.apply_lut_to_cartoon_face = lut_on_cartoon_face
+        self.only_seg = only_seg
+        self.face_bbox_result = face_bbox_result
+        if face_bbox_result is not None:
+            self.face_bbox_result = json.load(open(face_bbox_result))
+
+    def get_gaussian_filter(self):
+        heatmap = np.zeros((224, 224))
+        variance = 200
+        mul = 1.0
+        height, width = heatmap.shape
+        c_x, c_y = (112, 112)
+        for x_p in range(width):
+            for y_p in range(height):
+                dist_sq = (x_p - c_x) * (x_p - c_x) + \
+                          (y_p - c_y) * (y_p - c_y)
+                exponent = dist_sq / 2.0 / variance / variance
+                new_val = np.exp(-exponent) * mul
+                new_val = min(1, max(0, new_val))
+                heatmap[y_p, x_p] = new_val
+        return heatmap
+
+    def __call__(self, frame):
         frame = frame.copy()
-        if detect_face:
-            bboxes, _ = self.fd(frame)
-            if len(bboxes) != 0:
-                box = bboxes[0]
+        if self.apply_detect_face:
+            if self.face_bbox_result is not None:
+                box = self.face_bbox_result[self.call_counter]
+                # x, y, w, h => xmin, ymin, xmax, ymax
+                box[2] = box[0] + box[2]
+                box[3] = box[1] + box[3]
                 crop_face = frame[box[1]:box[3], box[0]:box[2], :]
+            else:
+                bboxes, _ = self.fd(frame)
+                if len(bboxes) != 0:
+                    box = bboxes[0]
+                    crop_face = frame[box[1]:box[3], box[0]:box[2], :]
         else:
             crop_face = frame
 
-        if seg:
+        if self.apply_seg:
             # skin include neck
             # mask_head = self.sehead(crop_face)
             # mask_skin = self.seskin(crop_face)
             # mask = (mask_head + mask_skin) / 2.0
 
-            # only head
-            mask = self.sehead(crop_face)
-            mask[mask >= 0.3] = 1.0
-            mask = cv2.blur(mask, (3, 3))
-            crop_face = (crop_face * mask[:, :, np.newaxis] + 255 * (1 - mask[:, :, np.newaxis])).astype(np.uint8)
+            # 如果使用了高斯模糊核作为 seg，就不需要推断 sehead
+
+            if self.only_seg:
+                # mask = self.get_gaussian_filter()
+                # mh, mw, _ = crop_face.shape
+                # mask = cv2.resize(mask, (mw, mh))
+                # mask_ori = self.sehead(crop_face)
+                # mask = mask_ori * mask
+                mask = self.sehead(crop_face)
+                mask[mask >= 0.8] = 1.0
+                mask = cv2.blur(mask, (5, 5))
+            else:
+                # only head
+                mask = self.sehead(crop_face)
+                mask[mask >= 0.3] = 1.0
+                mask = cv2.blur(mask, (5, 5))
+                crop_face = (crop_face * mask[:, :, np.newaxis] + 255 * (1 - mask[:, :, np.newaxis])).astype(np.uint8)
 
         face_cartoon = self.infer(crop_face)
         height, width, _ = face_cartoon.shape
@@ -173,22 +237,30 @@ class FaceCartoon:
         h_o = (exp_hei - height) // 2
         w_o = (exp_wid - width) // 2
         face_cartoon = face_cartoon[h_o:(height - h_o), w_o:(width - w_o)]
-        face_cartoon = cv2.resize(face_cartoon, (width, height))
+        face_cartoon = cv2.resize(face_cartoon, (width, height), interpolation=cv2.INTER_LANCZOS4)
 
-        if blend is False:
-            return face_cartoon
+        if self.apply_blend is False:
+            return cv2.resize(face_cartoon, (self.inp_w, self.inp_h), interpolation=cv2.INTER_LANCZOS4)
 
-        if proc_bg and self.custom_bg_func is not None:
+        if self.apply_proc_bg and self.custom_bg_func is not None:
             frame = self.custom_bg_func(frame)
-        elif proc_bg and self.lut_img is not None:
+        elif self.apply_proc_bg and self.lut_img is not None:
             frame = self.cartoon_bg(frame)
             frame = self.bg_lut(frame)
         else:
             frame = self.cartoon_bg(frame)
 
-        bg_cart = frame[box[1]:box[3], box[0]:box[2], :]
-        overlay_cartoon_face = (1 - mask[:, :, np.newaxis]) * bg_cart + mask[:, :, np.newaxis] * face_cartoon
-        overlay_cartoon_face = overlay_cartoon_face.astype(np.uint8)
+        if self.apply_seg:
+            bg_cart = frame[box[1]:box[3], box[0]:box[2], :]
+            overlay_cartoon_face = (1 - mask[:, :, np.newaxis]) * bg_cart + mask[:, :, np.newaxis] * face_cartoon
+            overlay_cartoon_face = overlay_cartoon_face.astype(np.uint8)
+        else:
+            overlay_cartoon_face = face_cartoon
+
+        if self.apply_lut_to_cartoon_face:
+            overlay_cartoon_face = self.cartoon_bg(overlay_cartoon_face)
+
         frame[box[1]:box[3], box[0]:box[2], :] = overlay_cartoon_face
 
+        self.call_counter += 1
         return frame
