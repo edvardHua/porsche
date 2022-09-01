dba860edc9acf89832b0863751d16e7012556457
zihua.zeng <zihua.zeng@shopee.com>
2022-09-01 16:12:46 +0800

    [+] portrait seg

--- porsche/face/segmentation.py
+++ porsche/face/segmentation.py
@@ -15,20 +15,35 @@ from porsche.utils.army_knife import get_porsche_base_path
 
 
 class Segmentation:
-
     MODEL_HEAD = os.path.join(get_porsche_base_path(), "models/HeadSeg.onnx")
     MODEL_SKIN = os.path.join(get_porsche_base_path(), "models/GlobalSkinSeg.onnx")
+    MODEL_PORTRAIT = os.path.join(get_porsche_base_path(), "models/PortraitSeg.onnx")
 
     def __init__(self, type="head", model_path=None):
+        self.type = type
         if model_path is None:
             if type == "head":
                 self.interpret = onnxruntime.InferenceSession(self.MODEL_HEAD)
-            else:
+            elif type == "skin":
                 self.interpret = onnxruntime.InferenceSession(self.MODEL_SKIN)
+            elif type == "portrait":
+                self.interpret = onnxruntime.InferenceSession(self.MODEL_PORTRAIT)
         else:
             self.interpret = onnxruntime.InferenceSession(model_path)
         # 这里宽和高是相等的
         self.side_height, self.side_width = self.interpret.get_inputs()[0].shape[2:]
+
+    def normalize_img(self, imgOri, scale, mean, val):
+        img = np.array(imgOri.copy(), np.float32) / scale
+        if len(img.shape) == 4:
+            for j in range(img.shape[0]):
+                for i in range(len(mean)):
+                    img[j, :, :, i] = (img[j, :, :, i] - mean[i]) * val[i]
+            return img
+        else:
+            for i in range(len(mean)):
+                img[:, :, i] = (img[:, :, i] - mean[i]) * val[i]
+            return img
 
     def infer(self, img, threshold=None, padding=True):
         ori_h, ori_w, _ = img.shape
@@ -40,14 +55,23 @@ class Segmentation:
 
         tmp_img = cv2.resize(pad_img, (self.side_width, self.side_height))
 
-        inp_img = tmp_img / 127.5 - 1
+        if self.type == "portrait":
+            inp_img = self.normalize_img(tmp_img, 1.0,
+                                         [103.94, 116.78, 123.68],
+                                         [0.017, 0.017, 0.017])
+        else:
+            inp_img = tmp_img / 127.5 - 1
+
         inp_img = inp_img.transpose((2, 0, 1))
         inp_img = inp_img[np.newaxis, :, :, :]
-
         inp_img = inp_img.astype(np.float32)
+
         pred = self.interpret.run(
             [self.interpret.get_outputs()[0].name],
             {self.interpret.get_inputs()[0].name: inp_img})[0]
+
+        if self.type == "portrait":
+            pred = pred[0, 1, ::]
 
         if threshold is not None:
             pred[pred < threshold] = 0.0
@@ -65,7 +89,6 @@ class Segmentation:
 
     def __call__(self, frame, threshold=None, padding=True):
         return self.infer(frame, threshold, padding)
-
 
 if __name__ == '__main__':
     se = Segmentation()
