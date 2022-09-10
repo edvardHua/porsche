3238aa5295db2d3abc5570b71b086ab7c1d46d6a
edvardzeng <edvard_hua@live.com>
2022-09-11 00:18:25 +0800

    modify upload shell

--- upload_package.sh
+++ upload_package.sh
@@ -1,3 +1,24 @@
-python3 setup.py sdist
-python3 setup.py sdist upload
-python3 setup.py register
+python_model_check()
+{
+  if python3 -c "import $1" >/dev/null 2>&1
+  then
+      echo "1"
+  else
+      echo "0"
+  fi
+}
+
+result=`python_model_check twine`
+
+if [ $result == 1 ]
+then
+  echo "twine already install"
+else
+  echo "Install twine package"
+  pip3 install twine
+fi
+
+python3 setup.py sdist build
+python3 -m  twine upload dist/*
+
+
